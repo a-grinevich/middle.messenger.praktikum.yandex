6f3e485895b6a425f214c85a82a96965a7a0a53a
Alexander Grinevich <a.b.grinevich@gmail.com>
2024-10-28 10:12:46 +0500

    + Добавлены заготовки страниц + Реализован переход между страницами

--- src/App.js
+++ src/App.js
@@ -1,19 +1,100 @@
-import Handlebars from 'handlebars';
-import * as Pages from './pages';
-import { mockPages } from './mockData.js';
+import Handlebars from "handlebars";
+import * as Pages from "./pages";
+import * as Components from "./components";
+import { mockPages } from "./mockData.js";
+import "./helpers/handlebarsHelpers.js";
+
+// Register partials
+Object.entries(Components).forEach(([name, component]) => {
+  Handlebars.registerPartial(name, component);
+});
 
 export default class App {
   constructor() {
     this.state = {
-      currentPage: 'navigation'
+      currentPage: this.getCurrentRoute(),
     };
-    this.appElement = document.getElementById('app');
+    this.pages = {
+      navigation: {
+        title: "Навигация",
+        page: Pages.NavigationPage,
+        context: {
+          pages: mockPages,
+        },
+      },
+      login: {
+        title: "Авторизация",
+        page: Pages.LoginPage,
+      },
+      register: {
+        title: "Регистрация",
+        page: Pages.RegisterPage,
+      },
+      chat: {
+        title: "Список чатов и лента переписки",
+        page: Pages.ChatPage,
+      },
+      settings: {
+        title: "Настройки пользователя",
+        page: Pages.SettingsPage,
+      },
+      404: {
+        title: "Страница 404",
+        page: Pages.ErrorPage,
+        context: {
+          code: "404"
+        },
+      },
+      500: {
+        title: "Страница 5**",
+        page: Pages.ErrorPage,
+        context: {
+          code: "500"
+        },
+      },
+    };
+    this.appElement = document.getElementById("app");
   }
 
   render() {
-      const template = Handlebars.compile(Pages.NavigationPage);
-      this.appElement.innerHTML = template({
-        pages: mockPages
+    const currentPage = this.pages[this.state.currentPage];
+    const template = Handlebars.compile(currentPage.page);
+    this.appElement.innerHTML = template(currentPage.context);
+    this.attachEventListeners();
+  }
+
+  attachEventListeners() {
+    const navigationLinks = document.querySelectorAll(".navigation-link");
+    navigationLinks.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.changePage(e.target.dataset.page);
       });
+    });
+    window.addEventListener(
+      "popstate",
+      () => {
+        this.navigate(this.getCurrentRoute());
+      },
+      false,
+    );
+  }
+
+  changePage(page) {
+    window.history.pushState({}, "", page);
+    this.navigate(page);
+  }
+
+  navigate(page) {
+    if (!page) {
+      page = "navigation";
+    }
+    this.state.currentPage = page;
+    this.render();
+  }
+
+  getCurrentRoute() {
+    const route = window.location.pathname.replace("/", "");
+    return route ? route : "navigation";
   }
 }
--- src/mockData.js
+++ src/mockData.js
@@ -1,8 +1,8 @@
 export const mockPages = [
-    {title: "Авторизация", link: "/login"},
-    {title: "Регистрация", link: "/register"},
-    {title: "Список чатов и лента переписки", link: "/chat"},
-    {title: "Настройки пользователя", link: "/settings"},
-    {title: "Страница 404", link: "/404"},
-    {title: "Страница 5**", link: "/500"}
-  ];
+  {title: "Авторизация", page: "login"},
+  {title: "Регистрация", page: "register"},
+  {title: "Список чатов и лента переписки", page: "chat"},
+  {title: "Настройки пользователя", page: "settings"},
+  {title: "Страница 404", page: "404"},
+  {title: "Страница 5**", page: "500"}
+];
